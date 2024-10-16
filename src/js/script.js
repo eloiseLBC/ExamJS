48fd365dfd2d29179c51e0c23bd838059fc6a039
eloiseLBC <eloise.leblanc@hotmail.com>
2024-10-16 11:16:24 +0200

    Trie caractere per house

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,9 @@
-async function loadTwelveCaracteres(){
-    const caracters = await fetch("https://hp-api.onrender.com/api/characters")
+async function loadTwelveCaracteres(house = null){
+    let url = "https://hp-api.onrender.com/api/characters/";
+    if (house != null){
+        url = "https://hp-api.onrender.com/api/characters/house/" + house;   
+    }
+    const caracters = await fetch(url)
         .then(data => data.json())
         .then(caracteres => {
             let i = 0;
@@ -10,7 +14,7 @@ async function loadTwelveCaracteres(){
                 console.log(caracteres[i]);
                 itemElement.className = "characters"
                 console.log(itemElement.image);
-                itemElement.innerHTML = "<img src='"+ caracteres[i].image+"' class='imgCharacters'></img>"                
+                itemElement.innerHTML = "<img src='"+ caracteres[i].image+"' class='imgCharacters' alt='"+ caracteres[i].name+"'></img>"                
                 document.querySelector(".characters").appendChild(itemElement);
                 i++;
             }
@@ -46,9 +50,20 @@ characters.forEach(image => {
 
 function createHouses(){
     let houses = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"];
-    
+    let contHouses = document.querySelector(".houses");
+    for (const house of houses){
+        let div = document.createElement("div");
+        div.addEventListener("click", function(){
+            const mainElement = document.querySelector(".characters");
+            mainElement.innerHTML = "";
+            loadTwelveCaracteres(house);
+        })
+
+        div.innerHTML = `<img src="./images/logo/` +house+ `.png">`;
+        contHouses.appendChild(div);
+    }
 }
 
 
-
+createHouses();
 loadTwelveCaracteres();
